82d6cb6dccfe7481e0c4ac22de2fce9bc67b3087
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-09-10 16:11:52 +0530

    fibonacci series till 1000

--- fibonacci_yield.rb
+++ fibonacci_yield.rb
@@ -1,10 +1,9 @@
 def fib(limit)
-	puts num1 = 0 
+	puts num1 = 1 
 	puts num2 = 1
 	until num1+num2 > limit do 
 		yield(num1,num2)
-		num1,num2 = num2,num1
-		num2 = num1+num2
+		num1,num2 = num2,num1+num2
 	end
 end
 
