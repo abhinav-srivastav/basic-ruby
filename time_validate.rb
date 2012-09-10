0dd8fa432377898795a83f0e5c472e534a0bce93
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-09-10 13:07:52 +0530

    add two time and validate using regex

--- time_validate.rb
+++ time_validate.rb
@@ -1,45 +1,36 @@
 
 def reg_validate(time)
-
 	exp = /(1 day & )?[0-23][:[0-59]]{2}/i
 	puts time if time =~ exp
 end
 
 
 def time_validate(time1, time2)
-
 	time1 = time1.split(':').reverse
 	time2 = time2.split(':').reverse
 	time_add = Array.new(3)
 	time_add.fill(0)
-	
 	l = 60
 	day = ""
 	time1.each_index do |x|
-		
-		if x == 2 
+		if x == 2
 			l = 24
 			time_add[x] += (time1[x].to_i + time2[x].to_i + time_add[x])%l
 			day = ((time1[x].to_i + time2[x].to_i)/l).to_s
-		else 
-	 		time_add[x] += (time1[x].to_i + time2[x].to_i + time_add[x])%l
+		else
+			time_add[x] += (time1[x].to_i + time2[x].to_i + time_add[x])%l
 			time_add[x+1] += (time1[x].to_i + time2[x].to_i )/l
 		end
-			
-		 	
 	end
-
 	time_add.each_index do |t|
-	time_add[t] = time_add[t].to_s.rjust(2,'0')
+		time_add[t] = time_add[t].to_s.rjust(2,'0')
 	end
-
-	time_add =  time_add.reverse.join(":")
-	
+	time_add = time_add.reverse.join(":")
 	time_add = day + " day & " + time_add unless day == '0'
 	reg_validate(time_add)
 end
 
-puts "Enter the time in hh:mm:ss" 
+puts "Enter the time in hh:mm:ss"
 time1 = gets.chomp
 
 puts "Enter the time to add in hh:mm:ss"
